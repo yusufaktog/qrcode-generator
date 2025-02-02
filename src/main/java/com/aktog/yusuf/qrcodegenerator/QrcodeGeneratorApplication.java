551af60a494350940ab24b86bc0fd8805775ce1b
yusufaktog <yusufaktok@gmail.com>
2025-02-02 13:54:13 +0300

    init project

---
+++ src/main/java/com/aktog/yusuf/qrcodegenerator/QrcodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.aktog.yusuf.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aktog/yusuf/qrcodegenerator/QrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktog.yusuf.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
