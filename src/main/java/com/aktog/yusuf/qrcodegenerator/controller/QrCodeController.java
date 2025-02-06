c538ab1072995c3da4be690781177406683eea2a
yusufaktog <yusufaktok@gmail.com>
2025-02-06 22:37:15 +0300

    cross origin added

--- src/main/java/com/aktog/yusuf/qrcodegenerator/controller/QrCodeController.java
+++ src/main/java/com/aktog/yusuf/qrcodegenerator/controller/QrCodeController.java
@@ -2,12 +2,10 @@ package com.aktog.yusuf.qrcodegenerator.controller;
 
 import com.aktog.yusuf.qrcodegenerator.service.QrCodeService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class QrCodeController {
     private final QrCodeService qrCodeService;
 
