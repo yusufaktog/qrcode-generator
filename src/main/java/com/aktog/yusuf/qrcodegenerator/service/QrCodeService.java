5e4ca833d0b1f4e1768263e6c6fc0ae54265cd4f
yusufaktog <yusufaktok@gmail.com>
2025-02-11 23:20:00 +0300

    added utf support

--- src/main/java/com/aktog/yusuf/qrcodegenerator/service/QrCodeService.java
+++ src/main/java/com/aktog/yusuf/qrcodegenerator/service/QrCodeService.java
@@ -2,6 +2,7 @@ package com.aktog.yusuf.qrcodegenerator.service;
 
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
@@ -10,15 +11,19 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class QrCodeService {
     public byte[] generateQrCode(String text, int width, int height) {
+
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix;
         ByteArrayOutputStream pngOutputStream;
+        Map<EncodeHintType, String> encodeHints = Map.of(EncodeHintType.CHARACTER_SET, "UTF-8");
         try {
-            bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+            bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, encodeHints);
             pngOutputStream = new ByteArrayOutputStream();
             MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
 
